a8000aa6cc6711936d9dc13fdf25a63a5f3e4614
Anas <88752225+aj-jum@users.noreply.github.com>
2024-01-11 06:19:23 -0500

    Implement Tail

---
+++ tail.js
@@ -0,0 +1,16 @@
+// Function implementation
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}` : `🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//Tail Function
+const tail = function(arrTail) {
+  arrTail.slice[1];
+  return arrTail;
+};
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
