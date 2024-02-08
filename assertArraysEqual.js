eed3e70ed56f7fdf282fa1294bb13d7198e706fd
Anas <88752225+aj-jum@users.noreply.github.com>
2024-02-08 07:38:20 -0500

    Implemnt the assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    // If lengths are not equal, return false immediately
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      // If any pair of elements at the same index are not equal, return false
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const message = eqArrays(array1, array2) ? "✅ Assertion Passed: Arrays are equal" : "🛑 Assertion Failed: Arrays are not equal";
+  console.log(message);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+assertArraysEqual([1, 2, 3], [3, 2, 1]); 
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); 
