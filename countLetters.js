43b15393ad7e746c2bb13e895c34c8539b72f3b0
Anas <88752225+aj-jum@users.noreply.github.com>
2024-02-10 16:40:31 -0500

    Implented countLetters function to input a word and then output the individual letter and letter count to a new object. Used assert equal to chectk test cases

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  console.log(actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}` : `🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(word) {
+  let result = {};
+  for (const letter of word) {
+  
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+         result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+const result1 = countLetters("sentence");
+assertEqual(result1["s"], 1);
+assertEqual(result1["e"], 3);
+assertEqual(result1["n"], 2);
+assertEqual(result1["c"], 2);
+assertEqual(result1["p"], 1);
--- countOnly.js
+++ countOnly.js
@@ -10,17 +10,19 @@ const countOnly = function(allItems, itemsToCount) {
     const results = {};
   
     for (const item of allItems) {
+
       if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
         results[item] = 1;
       }
-      console.log(item);
+      //console.log(item);
     }
   }
     return results;
-  };
+};
+
 const firstNames = [
   "Karl",
   "Salima",
