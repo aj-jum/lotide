c714e04e62e80c81c77d37f8ec75a524dc5eab79
Anas <88752225+aj-jum@users.noreply.github.com>
2024-02-15 09:29:30 -0500

    removed testcases into another file and added module.exports to the file

--- assertEqual.js
+++ assertEqual.js
@@ -3,6 +3,6 @@ const assertEqual = function(actual, expected) {
   console.log(actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}` : `🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+
+module.exports = assertEqual;
