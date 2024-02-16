f9ad6261c2d623aaba9b4da6991b0cebf77d37c1
Anas <88752225+aj-jum@users.noreply.github.com>
2024-02-16 09:02:22 -0500

    exporting utility functions from index.js so that test can reference index to import functions instead of individual files

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const head   = require('./head');
 const tail   = require('./tail');
-const middle = require('./middle')
+const middle = require('./middle');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  assertObjectsEqual: assertObjectsEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  takeUntil: takeUntil,
+  without: without
+
+
+
+
+
+
+
 };
