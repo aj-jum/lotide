dfe346381cf36126e42f27ae97ee26e3675572b4
Anas <88752225+aj-jum@users.noreply.github.com>
2024-02-16 01:31:35 -0500

    removed test case from original and added module.export. Also created test files by importing functions and adding test cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,6 +19,7 @@ const assertArraysEqual = function(array1, array2) {
   console.log(message);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
-assertArraysEqual([1, 2, 3], [3, 2, 1]); 
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); 
+ 
+
+
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -20,14 +20,6 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // should PASS
-assertEqual(eqArrays([], []), true); // should PASS
-assertEqual(eqArrays([1], [1]), true); // should PASS
-assertEqual(eqArrays([1], [2]), false); // should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // should PASS
+
 
 module.exports = eqArrays;
